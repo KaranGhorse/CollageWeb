6141a79ad83d16625376d94c19fca5965447213b
prashant dhote <prashant106dhote.github.io>
2024-02-21 12:33:50 +0530

    js code

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // statics and footer
+  let valuedisplay = document.querySelectorAll(".num")
+
+let interval= 4000;
+
+console.log(valuedisplay);
+
+valuedisplay.forEach((valuedisplay) => {
+    console.log(valuedisplay);
+    let startvalue = 0;
+    let endvalue = parseInt(valuedisplay.getAttribute("data-value"));
+    let duretion = Math.floor(interval /endvalue );
+    let counter = setInterval(()=>{
+        startvalue += 1 ;
+        valuedisplay.textContent = startvalue;
+        if(startvalue==endvalue){
+            clearInterval(counter);
+        }
+
+
+    },duretion)
+
+
+
+});
